2e5a0777ee04685960bbf3e2c68dcd7bb30da766
Yassel DG <yasseldg@gmail.com>
2023-08-13 10:53:22 +0000

    Update sJson Fixes

--- sJson/json.go
+++ sJson/json.go
@@ -20,65 +20,62 @@ func ToObj(msg string, obj any) error {
 func ToJson(v interface{}) (string, error) {
 	result, err := json.Marshal(v)
 	if err != nil {
+		sLog.Error("json.Marshal(v): %s", err)
 		return "", err
 	}
 	return string(result), nil
 }
 
-func Export(path string, objects any) (err error) {
-
+func Export(path string, objects any) error {
 	f, err := os.Create(path)
-
-	if err == nil {
-		defer f.Close()
-
-		data, err := json.MarshalIndent(objects, "", " ")
-		if err == nil {
-
-			b, err := f.Write(data)
-			if err == nil {
-
-				bb, err := f.WriteString("\n")
-				if err == nil {
-					sLog.Info("ExportJson: %d bytes written successfully in ( %s ). \n", (b + bb), path)
-				} else {
-					sLog.Error("ExportJson: f.WriteString(): path: %s : %s", path, err)
-				}
-			} else {
-				sLog.Error("ExportJson: f.Write(data): %s : %s", path, err)
-			}
-		} else {
-			sLog.Error("ExportJson: json.MarshalIndent(objects): %s : %s", path, err)
-		}
-	} else {
+	if err != nil {
 		sLog.Error("ExportJson: os.OpenFile( %s ) : %s", path, err)
+		return err
+	}
+	defer f.Close()
+
+	data, err := json.MarshalIndent(objects, "", " ")
+	if err != nil {
+		sLog.Error("ExportJson: json.MarshalIndent(objects): %s : %s", path, err)
+		return err
 	}
 
-	return
+	b, err := f.Write(data)
+	if err != nil {
+		sLog.Error("ExportJson: f.Write(data): %s : %s", path, err)
+		return err
+	}
+
+	bb, err := f.WriteString("\n")
+	if err != nil {
+		sLog.Error("ExportJson: f.WriteString(): path: %s : %s", path, err)
+		return err
+	}
+
+	sLog.Info("ExportJson: %d bytes written successfully in ( %s ). \n", (b + bb), path)
+	return nil
 }
 
-func Import(path string, objects any) (err error) {
-
+func Import(path string, objects any) error {
 	f, err := os.Open(path)
-
-	if err == nil {
-		defer f.Close()
-
-		data, err := io.ReadAll(f)
-		if err == nil {
-
-			err := json.Unmarshal(data, objects)
-			if err == nil {
-				sLog.Info("ImportJson: successful import of ( %s ). \n", path)
-			} else {
-				sLog.Error("ImportJson: json.Unmarshal(data, objects): %s", err)
-			}
-		} else {
-			sLog.Error("ImportJson: io.ReadAll(f): %s", err)
-		}
-	} else {
+	if err != nil {
 		sLog.Error("ImportJson: os.Open( %s ): %s", path, err)
+		return err
+	}
+	defer f.Close()
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		sLog.Error("ImportJson: io.ReadAll(f): %s", err)
+		return err
 	}
 
-	return
+	err = json.Unmarshal(data, objects)
+	if err != nil {
+		sLog.Error("ImportJson: json.Unmarshal(data, objects): %s", err)
+		return err
+	}
+
+	sLog.Info("ImportJson: successful import of ( %s ). \n", path)
+	return nil
 }
