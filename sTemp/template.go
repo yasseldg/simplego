7439342d357bf82a4e72e0a2102277f616618a46
Yassel DG <yasseldg@gmail.com>
2023-08-29 11:53:41 +0000

    sTemp LayoutWithFunc

--- sTemp/template.go
+++ sTemp/template.go
@@ -19,7 +19,11 @@ func Basics(dir_path string) Paths {
 }
 
 func Layout(w http.ResponseWriter, dir_temp_paths, path_prefix string, msg FlashMessages, temp_paths Paths, data Data) error {
-	tmpl, err := template.New("layout.html").Funcs(Functions()).ParseFiles(append(Basics(dir_temp_paths), temp_paths...)...)
+	return LayoutWithFunc(w, nil, dir_temp_paths, path_prefix, msg, temp_paths, data)
+}
+
+func LayoutWithFunc(w http.ResponseWriter, func_maps template.FuncMap, dir_temp_paths, path_prefix string, msg FlashMessages, temp_paths Paths, data Data) error {
+	tmpl, err := template.New("layout.html").Funcs(mergeFunctions(func_maps)).ParseFiles(append(Basics(dir_temp_paths), temp_paths...)...)
 	if err != nil {
 		sLog.Error(err.Error())
 		return err
@@ -35,4 +39,18 @@ func Layout(w http.ResponseWriter, dir_temp_paths, path_prefix string, msg Flash
 		return err
 	}
 	return nil
+}
+
+func mergeFunctions(func_maps template.FuncMap) template.FuncMap {
+	if func_maps == nil {
+		return Functions()
+	}
+
+	all_functions := Functions()
+	for k, v := range func_maps {
+		if _, ok := all_functions[k]; !ok {
+			all_functions[k] = v
+		}
+	}
+	return all_functions
 }
