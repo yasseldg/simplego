d787b731c666c491e87437af26cf07239d904e06
Yassel DG <yasseldg@gmail.com>
2023-10-25 18:26:23 +0000

    Update sSlice

--- sSlice/string.go
+++ sSlice/string.go
@@ -5,6 +5,10 @@ func InStrings(needle string, values []string) bool {
 }
 
 func InStringsPos(needle string, values []string) int {
+	if len(values) == 0 {
+		return -1
+	}
+
 	for i, v := range values {
 		if v == needle {
 			return i
@@ -19,4 +23,25 @@ func MapNames(values map[string]any) []string {
 		names = append(names, v)
 	}
 	return names
+}
+
+func DeleteStrings(values []string, needle string) []string {
+	if len(values) == 0 {
+		return values
+	}
+
+	pos := InStringsPos(needle, values)
+	if pos < 0 {
+		return values
+	}
+
+	if len(values) == 1 {
+		return []string{}
+	}
+
+	if pos == len(values)-1 {
+		return values[:pos]
+	}
+
+	return append(values[:pos], values[pos+1:]...)
 }
