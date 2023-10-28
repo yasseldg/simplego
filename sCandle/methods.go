b07191576c49efe58df15df66cd0a9fe04080008
Yassel DG <yasseldg@gmail.com>
2023-10-28 18:04:45 +0000

    Update sCandle

--- sCandle/methods.go
+++ sCandle/methods.go
@@ -1,8 +1,10 @@
 package sCandle
 
 import (
+	"fmt"
 	"math"
 
+	"github.com/yasseldg/simplego/sDate"
 	"github.com/yasseldg/simplego/sStat"
 )
 
@@ -11,4 +13,16 @@ func (c Candle) LogReturn() float64 {
 		return 0
 	}
 	return sStat.ValidFloat64(math.Log(c.Close / c.Open))
+}
+
+func (o OHLC) String(prec int) string {
+	return fmt.Sprintf("o: %*f  ..  h: %*f  ..  l: %*f  ..  c: %*f", prec, o.Open, prec, o.High, prec, o.Low, prec, o.Close)
+}
+
+func (o OHLCV) String(prec int) string {
+	return fmt.Sprintf("%s  ..  v: %*f", o.OHLC.String(prec), prec, o.Volume)
+}
+
+func (c Candle) String(prec int) string {
+	return fmt.Sprintf("Candle: %s  ..  %s", sDate.ForLog(c.UnixTs, 0), c.OHLCV.String(prec))
 }
